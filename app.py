f8aa90ab6ba0725b1782a316ccbf9cf436db10f1
Div3r <udhgjkf@gmail.com>
2024-07-01 15:30:49 -0600

    Solo falta navBar (che cbas pendejo)

--- app.py
+++ app.py
@@ -23,9 +23,21 @@ def index():
     return render_template('login.html')
 
 
-@app.route('/test')
-def test():
-    return render_template('modales/opcionesReceta.html')
+@app.route('/rec')
+def rec():
+    return render_template('vistas/recetasAdmin.html')
 
-if __name__ == '__main__':
+@app.route('/usu')
+def usu():
+    return render_template('vistas/usuariosAdmin.html')
+
+@app.route('/citas')
+def citas():
+    return render_template('vistas/citasAdmin.html')
+
+@app.route('/ex')
+def ex():
+    return render_template('vistas/expedientesAdmin.html')
+
+if __name__ == '__main__':  
     app.run(port=3000, debug=True)
